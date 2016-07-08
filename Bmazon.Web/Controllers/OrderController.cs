a75d08472a424615d627aec1bee037a90d5ffd8a
LeaderTNT <trong.tin.le.cs@gmail.com>
2016-07-08 14:25:56 -0400

    Use AJAX to update order status

--- Bmazon.Web/Controllers/OrderController.cs
+++ Bmazon.Web/Controllers/OrderController.cs
@@ -22,116 +22,84 @@ namespace Bmazon.Web.Controllers
             return View(orders.ToList());
         }
 
-        // GET: Order/Details/5
-        public ActionResult Details(int? id)
+        //// GET: Order/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Order order = db.Orders.Find(id);
+        //    if (order == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(order);
+        //}
+
+        //// GET: Order/Create
+        //public ActionResult Create()
+        //{
+        //    ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName");
+        //    ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName");
+        //    return View();
+        //}
+
+        //// POST: Order/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "OrderID,CustomerEmail,SellerEmail,TotalPayment")] Order order)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Orders.Add(order);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.CustomerEmail);
+        //    ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.SellerEmail);
+        //    return View(order);
+        //}
+
+        //// GET: Order/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Order order = db.Orders.Find(id);
+        //    if (order == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(order);
+        //}
+
+        //// POST: Order/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Order order = db.Orders.Find(id);
+        //    db.Orders.Remove(order);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+       [System.Web.Services.WebMethod]
+        public bool UpdateStatus(int id, int statusValue)
         {
-            if (id == null)
+            using (var ctx = new BmazonDbContext())
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                var result = ctx.Database.ExecuteSqlCommand($"UPDATE [Order] SET Status = {statusValue} WHERE OrderID = {id}");
+
+                return result == 1;
             }
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
         }
 
-        // GET: Order/Create
-        public ActionResult Create()
-        {
-            ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName");
-            ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName");
-            return View();
-        }
-
-        // POST: Order/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "OrderID,CustomerEmail,SellerEmail,TotalPayment")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Orders.Add(order);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.CustomerEmail);
-            ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.SellerEmail);
-            return View(order);
-        }
-
-        // GET: Order/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.CustomerEmail);
-            ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.SellerEmail);
-            return View(order);
-        }
-
-        // POST: Order/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "OrderID,CustomerEmail,SellerEmail,TotalPayment")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(order).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CustomerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.CustomerEmail);
-            ViewBag.SellerEmail = new SelectList(db.Accounts, "Email", "FirstName", order.SellerEmail);
-            return View(order);
-        }
-
-        // GET: Order/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = db.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
-        }
-
-        // POST: Order/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Order order = db.Orders.Find(id);
-            db.Orders.Remove(order);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
